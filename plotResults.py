b3a9344763b2eb1bc8f5266dde9bd3e207494b6b
davegabe <me@davegabe.it>
2023-02-10 16:39:21 +0100

    Small fixes

--- plotResults.py
+++ plotResults.py
@@ -1,23 +1,19 @@
-#get the options
 import sys
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 import json
 from pprint import pprint
-import numpy
 import numpy as np
+
 
 def parseFile():
     pathFile = sys.argv[1]
     print("The path file is: " + pathFile)
 
-
     scenarioToDiz = {}
 
     with open(pathFile) as f:
-        #read two lines at a time
+        # read two lines at a time
         for line1, line2 in zip(f, f):
             info = line1.split()
             scenarioName = info[1]
@@ -25,11 +21,10 @@ def parseFile():
             if scenarioName not in scenarioToDiz:
                 scenarioToDiz[scenarioName] = {}
             scenarioToDiz[scenarioName][threshold] = json.loads(line2)
-            #the second line should be read as a json
-    #pprint(scenarioToDiz)
+            # the second line should be read as a json
+    # pprint(scenarioToDiz)
 
     return scenarioToDiz
-
 
 
 def FARandFRR(info):
@@ -43,30 +38,18 @@ def FARandFRR(info):
             n_genuine_faces = info[scenario][threshold]["n_genuine_faces"]
             n_impostor_faces = info[scenario][threshold]["n_impostor_faces"]
             thresholds[threshold].append((far, frr, n_genuine_faces, n_impostor_faces))
-        
 
     pprint(thresholds)
 
     for threshold in thresholds:
-        far = 0
-        frr = 0
-        n_genuine_faces = 0
-        n_impostor_faces = 0
-        for far_, frr_, n_genuine_faces_, n_impostor_faces_ in thresholds[threshold]:
-            far += far_ * n_impostor_faces_
-            frr += frr_ * n_genuine_faces_
-            n_genuine_faces += n_genuine_faces_
-            n_impostor_faces += n_impostor_faces_
-        far /= n_impostor_faces
-        frr /= n_genuine_faces
-        thresholds[threshold] = (far, frr)
+        avg_far = np.mean([x[0] for x in thresholds[threshold]])
+        avg_frr = np.mean([x[1] for x in thresholds[threshold]])
+        thresholds[threshold] = (avg_far, avg_frr)
     return thresholds
 
 
-
 def plotFARandFRR(thresholds):
-    
-    #plot the results in the same plot
+    # plot the results in the same plot
     plt.figure()
     plt.title("FAR and FRR for different thresholds")
     plt.xlabel("Threshold")
@@ -92,40 +75,20 @@ def computeERR(threshold):
 
 
 if __name__ == "__main__":
-    info = parseFile()  
+    info = parseFile()
 
     thresholds = FARandFRR(info)
     plotFARandFRR(thresholds)
-    pprint(thresholds)  #TODO: scrivere l'output nel report
-    #compute the EER
+    pprint(thresholds)  # TODO: scrivere l'output nel report
+    # compute the EER
     EER = computeERR(thresholds)
-    #print("The EER is: " + str(EER))
+    # print("The EER is: " + str(EER))
 
-    #TODO: plottare la dir? magari la tronchiamo
+    # TODO: plottare la dir? magari la tronchiamo
 
-    #TODO: plottare la FRR e la FAR per ogni scenario
+    # TODO: plottare la FRR e la FAR per ogni scenario
 
     for scenario in info:
         thresholds = FARandFRR({scenario: info[scenario]})
         plotFARandFRR(thresholds)
         pprint(thresholds)
-        
-
-
-
-
-
-
-
-
-
-            
-
-
-
-            
-            
-            
-
-
-
